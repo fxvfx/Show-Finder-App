1dec13e7855070ddac760553a76d1f170cda9e77
Hannah Killian <hannahkillian@Hannahs-MacBook-Pro-3.local>
2020-04-05 16:36:41 -0400

    added review.java

---
+++ src/Review.java
@@ -0,0 +1,21 @@
+public class Review {
+
+	private String reviewContent;
+	private int numStars;
+	private UserAccount owner;
+
+	public static void main(String[] args) {
+
+
+	}
+
+	// Review
+	/**
+	 * @Override
+	 * something
+	 */
+
+	public String toString() {
+		return " ";
+	}
+}
