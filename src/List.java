20a92a47e6600b7d32e60fa92d99199a1ffd3fb1
Hannah Killian <hannahkillian@Hannahs-MacBook-Pro-3.local>
2020-04-05 23:13:31 -0400

    added list.java

---
+++ src/List.java
@@ -0,0 +1,14 @@
+
+public interface List {
+
+	public double CalculateTotal();
+
+	public String toString();
+
+	public void addItem();
+
+	public void removeItem();
+
+	public abstract getNext();
+
+}
