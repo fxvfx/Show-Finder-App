b0d32286335ada34ce42dda02b3403450fcb182f
Hannah Killian <hannahkillian@Hannahs-MacBook-Pro-3.local>
2020-04-06 15:41:28 -0400

    added TicketList.java

---
+++ src/TicketList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class TicketList {
+
+	public static void main(String args[]) {
+
+		// tickets: LinekdList<Ticket>
+		// I dont really understand what it means to just implement
+		// the linked list class
+        LinkedList<String> Ticket = new LinkedList<tickets>();
+	}
+
+	// getNext(): Ticket
+	public getNext(Ticket tickets) {
+		this.tickets = tickets;
+	}
+}
