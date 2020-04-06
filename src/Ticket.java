6ad2911458c07bcf6c2101a0f3bdf664d0a754b3
Hannah Killian <hannahkillian@Hannahs-MacBook-Pro-3.local>
2020-04-06 15:44:28 -0400

    added Ticket.java

---
+++ src/Ticket.java
@@ -0,0 +1,12 @@
+
+public class Ticket {
+
+	private String showName;
+	private String showTime;
+	private String seatID;
+	private double price;
+
+	public String toString() {
+		return " ";
+	}
+}
