4b380781e36ee7d0af973bf4b76dc66bd111beab
Hannah Killian <hannahkillian@Hannahs-MacBook-Pro-3.local>
2020-04-06 15:54:30 -0400

    added SeatTracker.java

---
+++ src/SeatTracker.java
@@ -0,0 +1,32 @@
+
+public class SeatTracker {
+
+	private TicketList takenSeats;
+	private TicketList availableSeats;
+	private TicketList handicappedSeats;
+
+	private takenSeats getSeats;
+	private availableSeats getAvailableSeats;
+	private handicappedSeats getHandicappedSeats;
+
+	public showAvailableSeats(SeatDisplay seats) {
+
+	}
+
+	public void buyTicket(Show shows) {
+
+	}
+
+	//idk what goes in (...)
+	public void buyTicket() {
+
+	}
+
+	public void buyTheater(Show shows) {
+
+	}
+
+	public void refundTicket() {
+
+	}
+}
