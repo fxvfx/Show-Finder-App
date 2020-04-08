a840e00c6530a8838b69d524e997a943fa291109
Brody Norton <mbnorton@email.sc.edu>
2020-04-08 03:55:09 -0400

    Added mpaRating

--- src/shows/Movie.java
+++ src/shows/Movie.java
@@ -14,19 +14,21 @@ public class Movie extends Show {
 	private String movieSummary;
 	private String releaseDate;
 	private String directorName;
+	private MovieRating mpaRating;
 	
 	public Movie(ReviewList reviews, String name, String showTime, String showDate, String venueName,
 			String location, double ticketPrice, LinkedList<String> actorList, int ageRestriction, 
-			String movieSummary, String releaseDate, String directorName) {
+			String movieSummary, String releaseDate, String directorName, MovieRating mpaRating) {
 		super(reviews, name, showTime, showDate, venueName, location, ticketPrice,
 				actorList, ageRestriction);
 		this.movieSummary = movieSummary;
 		this.releaseDate = releaseDate;
 		this.directorName = directorName;
+		this.mpaRating = mpaRating;
 	}
 	
 	public String toString() {
 		return super.toString() + "\nMovie Summary: " + movieSummary 
-				+ "\nRelease Date: " + releaseDate + "\nDirector: " + directorName;
+				+ "\nRelease Date: " + releaseDate + "\nDirector: " + directorName + "Rating" + mpaRating;
 	}
 }
---
+++ src/shows/MovieRating.java
@@ -0,0 +1,14 @@
+package shows;
+
+/**
+ * Enumerates MPAA ratings for movies
+ * @author Michael Norton (CSCE 247-001) //TODO team name
+ *
+ */
+public enum MovieRating {
+G,
+PG,
+PG13,
+R,
+NC17
+}
